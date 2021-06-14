8e9a3a1ba2016541aeebfd261298ba6d4898bccd
tnagorra <weathermist@gmail.com>
2021-06-14 13:36:38 +0545

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Toggle Request
+
+Request library by Togglecorp
+
+## Get Started
+
+```bash
+# Install dependencies
+yarn install
+
+# Build
+yarn build
+```
+
+## Linting
+
+```bash
+# Eslint
+yarn lint
+
+# Typescript
+yarn typecheck
+```
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export type { ContextInterface } from './context';
+export { default as RequestContext } from './context';
+export type { UrlParams } from './types';
+export { default as useLazyRequest } from './useLazyRequest';
+export type { LazyRequestOptions } from './useLazyRequest';
+export { default as useRequest } from './useRequest';
+export type { RequestOptions } from './useRequest';
+export { prepareUrlParams } from './utils';
+export type { Methods } from './utils';
